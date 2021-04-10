81c835b9e64e64ea95db6fda7c3b9ae6d620ed82
Vinicius Ávila <viniciusvaln10@hotmail.com>
2021-04-10 20:02:00 +0000

    Selection Sort

---
+++ 07-selection-sort.mjs
@@ -0,0 +1,44 @@
+/*
+    Trata-se de uma otimização do bubble sort, diminuindo drasticamente o número de trocas necessárias para fazer a ordenação.
+    Isola um dos valores do vetor e procura pelo menor valor entre os restantes, promovendo a troca caso o primeiro valor seja maior que o segundo.
+*/
+let trocas, pass, comps
+
+function selectionSort(vetor) {
+    trocas = 0, pass = 0, comps = 0
+
+    function encontrarMenor(inicio) {
+        let menor = inicio
+        // Este loop vai até a última posição
+        for(let j = inicio + 1; j < vetor.length; j++) {
+            if(vetor[j] < vetor[menor]) menor = j
+            comps++
+            // Na passagem para função de comparação, a ordem dos objetos precisa ser invertida
+        }
+        return menor
+    }
+
+    // Percurso do vetor até a PENÚLTIMA posição
+    for(let i = 0; i <= vetor.length - 2; i++) {
+        pass++
+        let menor = encontrarMenor(i + 1)
+        if(vetor[menor] < vetor[i]) {
+        // Parâmetros em ordem invertida
+            [ vetor[menor], vetor[i] ] = [ vetor[i], vetor[menor] ]
+            trocas++
+        }
+        comps++
+    }
+}
+
+// Função de comparação retorna true caso o PRIMEIRO objeto seja MAIOR que o SEGUNDO
+
+import { candidatos } from './includes/candidatos-2018.mjs'
+
+//console.log('ANTES:', candidatos)
+console.time('Ordenando candidatos...')
+// Ordenando pelo nome de urna (NM_URNA_CANDIDATO)
+selectionSort(candidatos, (obj1, obj2) => obj1.NM_URNA_CANDIDATO > obj2.NM_URNA_CANDIDATO)
+console.timeEnd('Ordenando candidatos...')
+console.log('DEPOIS:', candidatos)
+console.log({trocas, pass, comps})
